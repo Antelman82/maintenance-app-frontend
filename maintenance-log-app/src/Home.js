b6f3167dc47d1db75cb83c721c21d59f9f0463b2
Tony Antelman <AntelmanAnthonyJ@JohnDeere.com>
2020-04-21 06:38:15 -0500

    home paragraph tags

--- maintenance-log-app/src/Home.js
+++ maintenance-log-app/src/Home.js
@@ -7,12 +7,12 @@ const Home = props => {
             <p>This web app was created to help vehicle owners manage and
                  track repairs, routine maintenance, and other important information for each vehicle they own.
                  The goal is to allow users an easy way to remember what has and hasn't been done.
-                 A typical problem people face is remembering the last time something was done to the vehicle.
+                 A typical problem people face is remembering the last time something was done to the vehicle.</p>
                  
-                 This also comes in handy when trying to sell a vehicle and the buy wants to know the last time
-                 the timing belt was replaced, or how often certain services were performed.
+            <p>This also comes in handy when trying to sell a vehicle and the buy wants to know the last time
+                 the timing belt was replaced, or how often certain services were performed.</p>
                  
-                 Note: this app isn't exclusive to cars or trucks, but could be used for boats, motorcycles, bicycles, and even your House.</p>
+            <p>Note: this app isn't exclusive to cars or trucks, but could be used for boats, motorcycles, bicycles, and even your House.</p>
         </div>
     )
 }
